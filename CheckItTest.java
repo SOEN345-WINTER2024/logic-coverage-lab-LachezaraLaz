318633d010332bc9777485ae07042c1ffbfc8f3f
LachezaraLaz <122994431+LachezaraLaz@users.noreply.github.com>
2024-03-15 12:37:23 -0400

    Update CheckItTest.java with step 3

--- CheckItTest.java
+++ CheckItTest.java
@@ -3,14 +3,28 @@ import static org.junit.Assert.*;
 
 public class CheckItTest {
 
+    //step 2
     @Test
-    public void testCheckItTrue(){
+    public void testCheckItTrue1(){
         assertTrue(CheckIt.checkIt(true, true, true));
     }
 
+    //step 2
     @Test
-    public void testCheckItFalse(){
+    public void testCheckItFalse1(){
         assertFalse(CheckIt.checkIt(false, false, true));
+    }
+
+    //step 3
+    @Test
+    public void testCheckItTrue2(){
+        assertTrue(CheckIt.checkIt(true, true, true));
+    }
+
+    //step 3
+    @Test
+    public void testCheckItFalse2(){
+        assertFalse(CheckIt.checkIt(false, false, false));
     }
 
 }
