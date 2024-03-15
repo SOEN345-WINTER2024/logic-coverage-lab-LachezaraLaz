434d944e630a3be91bef213870e000f15bc12547
LachezaraLaz <122994431+LachezaraLaz@users.noreply.github.com>
2024-03-15 12:51:22 -0400

    Update CheckItTest.java with step 4

--- CheckItTest.java
+++ CheckItTest.java
@@ -27,4 +27,51 @@ public class CheckItTest {
         assertFalse(CheckIt.checkIt(false, false, false));
     }
 
+    //step 4 test 1
+    @Test
+    public void CoCTest1(){
+        assertTrue(CheckIt.checkIt(true,true,true));
+    }
+
+    //step 4 test 2
+    @Test
+    public void CoCTest2(){
+        assertTrue(CheckIt.checkIt(true,true,false));
+    }
+
+    //step 4 test 3
+    @Test
+    public void CoCTest3(){
+        assertTrue(CheckIt.checkIt(true,false,true));
+    }
+
+    //step 4 test 4
+    @Test
+    public void CoCTest4(){
+        assertTrue(CheckIt.checkIt(true,false,false));
+    }
+
+    //step 4 test 5
+    @Test
+    public void CoCTest5(){
+        assertTrue(CheckIt.checkIt(false,true,true));
+    }
+
+    //step 4 test 6
+    @Test
+    public void CoCTest6(){
+        assertFalse(CheckIt.checkIt(false,true,false));
+    }
+
+    //step 4 test 7
+    @Test
+    public void CoCTest7(){
+        assertFalse(CheckIt.checkIt(false,false,true));
+    }
+
+    //step 4 test 8
+    @Test
+    public void CoCTest8(){
+        assertFalse(CheckIt.checkIt(false,false,false));
+    }
 }
